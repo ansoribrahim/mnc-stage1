c86b45d8491ae60aad6b2675a8b5103ff5f59246
ansari <ansoribra@gmail.com>
2025-01-18 09:43:14 +0700

    add implementation get change details

--- get_change_details.go
+++ get_change_details.go
@@ -1,5 +1,27 @@
 package main
 
 func GetChangeDetails(total, paid int) (map[int]int, int, bool) {
-	return make(map[int]int), 0, false
+	if paid < total {
+		return nil, 0, false
+	}
+
+	change := paid - total
+	roundedChange := (change / 100) * 100
+
+	denominations := []int{100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100}
+
+	result := make(map[int]int)
+	remainingChange := roundedChange
+
+	for _, denom := range denominations {
+		if remainingChange >= denom {
+			quantity := remainingChange / denom
+			if quantity > 0 {
+				result[denom] = quantity
+				remainingChange = remainingChange % denom
+			}
+		}
+	}
+
+	return result, roundedChange, true
 }
