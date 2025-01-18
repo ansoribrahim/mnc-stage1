196d916db90f7886bdee2e33275cf6a0a440f454
ansari <ansoribra@gmail.com>
2025-01-18 10:01:01 +0700

    add implementation check brackets

--- check_brackets.go
+++ check_brackets.go
@@ -1,5 +1,29 @@
 package main
 
 func CheckBrackets(input string) bool {
-	return false
+	var stack []rune
+
+	pairs := map[rune]rune{
+		'>': '<',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, char := range input {
+		switch char {
+		case '<', '{', '[':
+			stack = append(stack, char)
+		case '>', '}', ']':
+			if len(stack) == 0 {
+				return false
+			}
+			last := stack[len(stack)-1]
+			expected, exists := pairs[last]
+			if !exists || expected != char {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
 }
